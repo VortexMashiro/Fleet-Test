5ba82e45cde8ded79f4e45d6069ed7e631559e12
VortexMashiro <mashiro@vortexmashiro.cc>
2018-10-16 13:24:23 +0800

    Update generalSettingsFrame.py

--- PyEveLiveDPS/settings/generalSettingsFrame.py
+++ PyEveLiveDPS/settings/generalSettingsFrame.py
@@ -29,17 +29,17 @@ class GeneralSettingsFrame(tk.Frame):
         
         self.secondsVar = tk.StringVar()
         self.secondsVar.set(settings.getSeconds())
-        self.addSetting(self.secondsVar, "每隔几秒计算一次平均DPS:", 
+        self.addSetting(self.secondsVar, "DPS计算周期(秒):", 
                         "建议将此条设置为比你武器循环时间高一点的整数")
         
         self.intervalVar = tk.StringVar()
         self.intervalVar.set(settings.getInterval())
-        self.addSetting(self.intervalVar, "多少毫秒绘制一次图形:", 
+        self.addSetting(self.intervalVar, "图形绘制周期(毫秒):", 
                         "数值越低CPU负担越大")
         
         self.transparencyVar = tk.StringVar()
         self.transparencyVar.set(settings.getCompactTransparency())
-        self.addSetting(self.transparencyVar, "简易模式的透明度:", 
+        self.addSetting(self.transparencyVar, "Compat模式的透明度:", 
                         "100为不透明，0为全透明")
         
         
@@ -62,54 +62,54 @@ class GeneralSettingsFrame(tk.Frame):
         try:
             secondsSetting = int(self.secondsVar.get())
         except ValueError:
-            tk.messagebox.showerror("Error", "Please enter a number for number of seconds to average DPS")
+            tk.messagebox.showerror("错误", "请输入DPS计算周期数值(秒)")
             return
         if (secondsSetting < 2 or secondsSetting > 600):
-            tk.messagebox.showerror("Error", "Please enter a value between 2-600 for number of seconds to average DPS")
+            tk.messagebox.showerror("错误", "请输入2-600之间的DPS计算周期数值")
             return  
         
         try:
             intervalSetting = int(self.intervalVar.get())
         except ValueError:
-            tk.messagebox.showerror("Error", "Please enter a number for milliseconds to update graph")
+            tk.messagebox.showerror("错误", "请输入图形绘制周期数值(毫秒)")
             return
         if (intervalSetting < 10 or intervalSetting > 1000):
-            tk.messagebox.showerror("Error", "Please enter a value between 10-1000 for milliseconds to update graph")
+            tk.messagebox.showerror("错误", "请输入10-1000之间的图形绘制周期数值")
             return
         
         if ((secondsSetting*1000)/intervalSetting <= 10):
-            tk.messagebox.showerror("Error", "(Seconds to average DPS*1000)/(Graph update interval) must be > 10.\n" +
-                                   "If it is less than 10, we won't have enough data to draw an accurate graph!")
+            tk.messagebox.showerror("错误", "DPS计算周期/图形绘制周期必须> 10.\n" +
+                                   "如果这个比例小于10，软件没有足够的数据来绘制图形!")
             return
         
         if ((secondsSetting*1000)/intervalSetting < 20):
-            okCancel = tk.messagebox.askokcancel("Continue?", "(Seconds to average DPS*1000)/(Graph update interval)\n is < 20\n" +
-                                   "This is ok, but it is recommended to increase your (Seconds to average DPS) or decrease your (Graph update interval) to improve your graphing experience.\n"
-                                   "Would you like to keep these settings?")
+            okCancel = tk.messagebox.askokcancel("你确定要继续吗?", "DPS计算周期/图形绘制周期\n  < 20\n" +
+                                   "这样虽然没有什么大问题,但是我们推荐提高你的DPS计算周期或者降低你的图形绘制周期来获得更好的图形体验.\n"
+                                   "你确定要保存这些设定吗?")
             if not okCancel:
                 return
             
         if (intervalSetting < 50):
-            okCancel = tk.messagebox.askokcancel("Continue?", "Setting the graph update interval to less than 50ms is generally a bad idea.  Your CPU won't like it."
-                                                 "Would you like to keep these settings?")
+            okCancel = tk.messagebox.askokcancel("你确定要继续吗?", "将图形绘制周期设置为小于50的数值会给CPU极大的压力，我们不推荐这样做."
+                                                 "你确定要保存这些设定吗?")
             if not okCancel:
                 return
             
         if (secondsSetting/intervalSetting > 1):
-            okCancel = tk.messagebox.askokcancel("Continue?", "(Seconds to average DPS)/(Graph update interval)\n is > 1\n" +
-                                   "This is ok, but it is recommended to decrease your (Graph update interval) to improve performance.\n"
-                                   "You don't need such a low graph update interval if you are using a high (Seconds to average DPS)\n"
-                                   "Would you like to keep these settings?")
+            okCancel = tk.messagebox.askokcancel("你确定要继续吗?", "DPS计算周期/图形绘制周期\n 现在 > 1\n" +
+                                   "这样虽然没有什么大问题,但是我们推荐降低你的图形绘制周期来获得更好的性能.\n"
+                                   "在DPS计算周期较高的时候你不需要如此低的绘制周期\n"
+                                   "你确定要保存这些设定吗?")
             if not okCancel:
                 return
             
         try:
             compactTransparencySetting = int(self.transparencyVar.get())
         except ValueError:
-            tk.messagebox.showerror("Error", "Please enter a number for compact transparency percentage")
+            tk.messagebox.showerror("错误", "请输入透明度数值")
             return
         if (compactTransparencySetting < 1 or compactTransparencySetting > 100):
-            tk.messagebox.showerror("Error", "Please enter a value between 1-100 for compact transparency percentage")
+            tk.messagebox.showerror("错误", "请输入1-100之间的透明度数值")
             return  
         
         return {"seconds": secondsSetting, "interval": intervalSetting, 
